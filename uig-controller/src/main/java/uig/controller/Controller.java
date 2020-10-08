e1a5abe6d19a1b55858b733634aeae39cf4aaed0
Verónica Campos <Ivxn@DESKTOP-7C9T7LJ>
2020-10-08 11:11:04 -0300

    Cambios al build.gradle

---
+++ uig-controller/src/main/java/uig/controller/Controller.java
@@ -0,0 +1,15 @@
+package uig.controller;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class Controller implements PropertyChangeListener{
+	
+
+	@Override
+	public void propertyChange(PropertyChangeEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
